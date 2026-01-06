ae9fe7084ca3d50114fe4b004ce595e79f4075bf
Z1TK <204508328+Z1TK@users.noreply.github.com>
2026-01-06 23:35:42 +0300

    Methods: mset, mget, incr, derc

--- commands.go
+++ commands.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"sync"
+	"strconv"
 )
 
 type KeyValuePair struct {
@@ -20,6 +21,18 @@ var Handlers = map[string]func(*KeyValuePair, []Value) Value {
 	"SET": set,
 	"GET": get,
 	"SETNX": setnx,
+	"MSET": mset,
+	"MGET": mget,
+	"INCR": incr,
+	"DECR": decr,
+	// "RPUSH": rpush,
+	// "LPUSH": lpush,
+	// "RPOP": rpop,
+	// "LPOP": lpop,
+	// "LRANGE": lrange,
+	// "DEL": del,
+	// "EXPIRE": expire,
+	// "TTL": ttl,
 }
 
 func ping(_ *KeyValuePair, args []Value) Value {
@@ -57,7 +70,6 @@ func get(kv *KeyValuePair, args []Value) Value {
 	defer kv.Mu.Unlock()
 
 	val, ok := kv.Strings[key]
-
 	if !ok {
 		return Value{typ: "null"}
 	}
@@ -75,11 +87,101 @@ func setnx(kv *KeyValuePair, args []Value) Value {
 
 	kv.Mu.Lock()
 	defer kv.Mu.Unlock()
-	
+
 	if _, exist := kv.Strings[key]; !exist {
 		kv.Strings[key] = val
 		return Value{typ: "integer", num: 1}
 	}
 
 	return Value{typ: "integer", num: 0}
+}
+
+func mset(kv *KeyValuePair, args []Value) Value {
+	if len(args) % 2 != 0 {
+		return Value{typ: "error", str: "wrong number of arguments for 'mset' command"}
+	}
+
+	kv.Mu.Lock()
+	defer kv.Mu.Unlock()
+
+	for i := 0; i < len(args); i += 2 {
+		key := args[i].bulk
+		val := args[i+1].bulk
+		kv.Strings[key] = val
+	}
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func mget(kv *KeyValuePair, args []Value) Value {
+	if len(args) < 1 {
+		return Value{typ: "error", str: "wrong number of arguments for 'mget' command"}
+	}
+
+	var res []Value
+	kv.Mu.Lock()
+	defer kv.Mu.Unlock()
+
+	for i := 0; i < len(args); i += 1 {
+		key := args[i].bulk
+		if val, exist := kv.Strings[key]; exist {
+			res = append(res, Value{typ: "bulk", bulk: val})
+		} else {
+			res = append(res, Value{typ: "null"})
+		}
+	}
+
+	return Value{typ: "array", array: res}
+}
+
+func incr(kv *KeyValuePair, args []Value) Value {
+	if len(args) != 1  {
+		return Value{typ: "error", str: "wrong number of arguments for 'incr' command"}
+	}
+
+	key := args[0].bulk
+
+	kv.Mu.Lock()
+	defer kv.Mu.Unlock()
+
+	if _, exist := kv.Strings[key]; !exist {
+		kv.Strings[key] = "1"
+		return Value{typ: "integer", num: 1}
+	}
+
+	n, err := strconv.Atoi(kv.Strings[key])
+	if err != nil {
+		return Value{typ: "error", str: "value is not an integer or out of range"}
+	}
+	n++
+
+	kv.Strings[key] = strconv.Itoa(n)
+
+	return Value{typ: "integer", num: n}
+}
+
+func decr(kv *KeyValuePair, args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "wrong number of arguments for 'decr' command"}
+	}
+
+	key := args[0].bulk
+
+	kv.Mu.Lock()
+	defer kv.Mu.Unlock()
+
+	if _, exist := kv.Strings[key]; !exist {
+		kv.Strings[key] = "-1"
+		return Value{typ: "integer", num: -1}
+	}
+
+	n, err := strconv.Atoi(kv.Strings[key])
+	if err != nil {
+		return Value{typ: "error", str: "value is not an integer or out of range"}
+	}
+	n--
+
+	kv.Strings[key] = strconv.Itoa(n)
+
+	return Value{typ: "integer", num: n}
 }
