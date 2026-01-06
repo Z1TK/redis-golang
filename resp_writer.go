1f3582acd34ef7165238a264a2341bb5fda48c2c
Z1TK <204508328+Z1TK@users.noreply.github.com>
2026-01-06 13:27:06 +0300

    Redis reply

--- resp_writer.go
+++ resp_writer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"io"
 )
 
@@ -25,18 +26,18 @@ func (w *respWriter) Write(v Value) error {
 
 func (v Value) replyValue() []byte {
 	switch v.typ {
-	// case "array":
-	// 	return v.replyArray()
-	// case "bulk":
-	// 	return v.replyBulk()
+	case "array":
+		return v.replyArray()
+	case "bulk":
+		return v.replyBulk()
 	case "string":
 		return v.replyString()
-	// case "error":
-	// 	return v.replyError()
-	// case "integer":
-	// 	return v.replyInteger()
-	// case "null":
-	// 	return v.replyNull()
+	case "error":
+		return v.replyError()
+	case "integer":
+		return v.replyInteger()
+	case "null":
+		return v.replyNull()
 	default:
 		return []byte{}
 	}
@@ -48,4 +49,49 @@ func (v Value) replyString() []byte {
 	bytes = append(bytes, v.str...)
 	bytes = append(bytes, '\r', '\n')
 	return bytes
+}
+
+func (v Value) replyArray() []byte {
+	var bytes []byte
+	length := len(v.array)
+	bytes = append(bytes, ARRAY)
+	bytes = append(bytes, strconv.Itoa(length)...)
+	bytes = append(bytes, '\r', '\n')
+
+	for i := 0; i < length; i++ {
+		bytes = append(bytes, v.array[i].replyValue()...)
+	}
+
+	return bytes
+}
+
+func (v Value) replyBulk() []byte {
+	var bytes []byte
+	length := len(v.bulk)
+	bytes = append(bytes, BULK)
+	bytes = append(bytes, strconv.Itoa(length)...)
+	bytes = append(bytes, '\r', '\n')
+	bytes = append(bytes, v.bulk...)
+	bytes = append(bytes, '\r', '\n')
+	return bytes
+}
+
+func (v Value) replyInteger() []byte {
+	var bytes []byte
+	bytes = append(bytes, INTEGER)
+	bytes = append(bytes, strconv.Itoa(v.num)...)
+	bytes = append(bytes, '\r', '\n')
+	return bytes
+}
+
+func (v Value) replyError() []byte {
+	var bytes []byte
+	bytes = append(bytes, ERROR)
+	bytes = append(bytes, v.str...)
+	bytes = append(bytes, '\r', '\n')
+	return bytes
+}
+
+func (v Value) replyNull() []byte {
+	return []byte("$-1\r\n")
 }
