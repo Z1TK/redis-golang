a2bf1a94d6cbc63f6a9da02e806adf44a2c54e67
Z1TK <204508328+Z1TK@users.noreply.github.com>
2026-01-06 20:11:01 +0300

    Methods: ping, set, get, setnx

--- commands.go
+++ commands.go
@@ -17,6 +17,9 @@ func createKVP() *KeyValuePair {
 
 var Handlers = map[string]func(*KeyValuePair, []Value) Value {
 	"PING": ping,
+	"SET": set,
+	"GET": get,
+	"SETNX": setnx,
 }
 
 func ping(_ *KeyValuePair, args []Value) Value {
@@ -25,4 +28,58 @@ func ping(_ *KeyValuePair, args []Value) Value {
 	}
 
 	return Value{typ: "string", str: args[0].bulk}
+}
+
+func set(kv *KeyValuePair, args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "wrong number of arguments for 'set' command"}
+	}
+
+	key := args[0].bulk
+	val := args[1].bulk
+
+	kv.Mu.Lock()
+	defer kv.Mu.Unlock()
+
+	kv.Strings[key] = val
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func get(kv *KeyValuePair, args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "wrong number of arguments for 'get' command"}
+	}
+
+	key := args[0].bulk
+
+	kv.Mu.Lock()
+	defer kv.Mu.Unlock()
+
+	val, ok := kv.Strings[key]
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	return Value{typ: "bulk", bulk: val}
+}
+
+func setnx(kv *KeyValuePair, args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "wrong number of arguments for 'set' command"}
+	}
+
+	key := args[0].bulk
+	val := args[1].bulk
+
+	kv.Mu.Lock()
+	defer kv.Mu.Unlock()
+	
+	if _, exist := kv.Strings[key]; !exist {
+		kv.Strings[key] = val
+		return Value{typ: "integer", num: 1}
+	}
+
+	return Value{typ: "integer", num: 0}
 }
